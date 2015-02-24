2ccdea67c9d19c88a0fa98f7e958211bc4596c7d
横山 優 <wasabiinu@wasabiinu-no-MacBook-Pro.local>
2015-02-25 08:23:25 +0900

    タイマーマネージャークラス追加

---
+++ OpenWorldQuarterViewRPG/manager/TimeManager.swift
@@ -0,0 +1,13 @@
+//
+//  TimeManager.swift
+//  OpenWorldQuarterViewRPG
+//
+//  Created by 横山 優 on 2015/02/24.
+//  Copyright (c) 2015年 Yu Yokoyama. All rights reserved.
+//
+
+import Foundation
+internal class TimerManager
+{
+    
+}
