b4ebab9584a00c258d549db733931b230d4e2793
横山 優 <wasabiinu@wasabiinu-no-MacBook-Pro.local>
2015-02-25 23:39:15 +0900

    TimeModel名称変更

---
+++ OpenWorldQuarterViewRPG/model/time/TimeModel.swift
@@ -0,0 +1,17 @@
+//
+//  TimerModel.swift
+//  OpenWorldQuarterViewRPG
+//
+//  Created by 横山 優 on 2015/02/25.
+//  Copyright (c) 2015年 Yu Yokoyama. All rights reserved.
+//
+
+import Foundation
+internal class TimeModel
+{
+    init ()
+    {
+        //主人公を動かすのを追加
+        TimeManager.add(UIUtil.delegate.moveHero, anyObjects: [MapUtil.delegate.getModelHero()])
+    }
+}
