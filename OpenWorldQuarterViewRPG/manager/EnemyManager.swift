fef6e7a2354ac6c0e94bf87319fc36a90efeab76
横山 優 <wasabiinu@wasabiinu-no-MacBook-Pro.local>
2015-03-02 02:08:27 +0900

    キャラクター素材と、エネミービュー

---
+++ OpenWorldQuarterViewRPG/manager/EnemyManager.swift
@@ -0,0 +1,17 @@
+//
+//  EnemyManager.swift
+//  OpenWorldQuarterViewRPG
+//
+//  Created by 横山 優 on 2015/02/28.
+//  Copyright (c) 2015年 Yu Yokoyama. All rights reserved.
+//
+
+import Foundation
+internal class EnemyManager
+{
+    internal var enemyArray:[Enemy]
+    init ()
+    {
+        enemyArray = [Enemy]()
+    }
+}
